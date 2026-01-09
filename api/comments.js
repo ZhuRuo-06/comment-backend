06f8ed6c0fb4698e3bf1d6c83ce6b0f146364a2f
ZhuRuo-06 <dony.083147726274@gmail.com>
2026-01-09 14:46:57 +0700

    Update comments.js

--- api/comments.js
+++ api/comments.js
@@ -1,3 +1,24 @@
+let comments = [];
+
 module.exports = (req, res) => {
-  res.status(200).json({ message: "API backend aktif" });
+  if (req.method === 'GET') {
+    return res.status(200).json(comments);
+  }
+
+  if (req.method === 'POST') {
+    const { name, message } = req.body;
+    if (!name || !message) {
+      return res.status(400).json({ error: 'Nama & pesan wajib' });
+    }
+
+    comments.push({
+      name,
+      message,
+      date: new Date().toISOString()
+    });
+
+    return res.status(200).json({ success: true });
+  }
+
+  res.status(405).json({ error: 'Method not allowed' });
 };
